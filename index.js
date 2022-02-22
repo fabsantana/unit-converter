d4324483af1588c7af4ef71b26e0ef26e38f5325
Fabricio <76725371+fabsantana@users.noreply.github.com>
2022-02-21 20:41:52 -0500

    HTML, CSS finished. JS kinda done.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-let conversionNumber = document.getElementById("conversion-input");
 
-function convert() {
-    function lengthConvert() {
+function convert () {
+    let conversionNumber = document.getElementById('conversion-input').value;
+    let metricToImperialLength = conversionNumber * 3.281
+    let metricToImperialMass = conversionNumber * 2.205
+    let metricToImperialVolume = conversionNumber / 3.78541
+    let ImperialToMetricLength = conversionNumber / 3.281
+    let ImperialToMetricVolume = conversionNumber * 3.78541
+    let ImperialToMetricMass = conversionNumber / 2.205
 
-    }
-    function volumeConvert() {
+    
+    document.getElementById("length-unit").textContent = conversionNumber + " meters = " + metricToImperialLength.toFixed(3) + " feet | " + conversionNumber + " feet = " + ImperialToMetricLength.toFixed(3) + " meters";
+    
+    document.getElementById("volume-unit").textContent = conversionNumber + " liters = " + metricToImperialVolume.toFixed(3) + " gallons | " + conversionNumber + " gallons = " + ImperialToMetricVolume.toFixed(3) + " liters";
 
-    }
-    function massConvert() {
+    document.getElementById("mass-unit").textContent = conversionNumber + " kilos = " + metricToImperialMass.toFixed(3) + " pounds | " + conversionNumber + " pounds = " + ImperialToMetricMass.toFixed(3) + " kilos";
 
-    }
 }
