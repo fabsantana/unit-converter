1b4348a74ea7764a8161009f6616cdff5be630fd
fabsantana <fabro715@gmail.com>
2022-03-19 15:53:41 -0400

    added js

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-document.getElementById("conversion-input").oninput = function limitCharacters() {
-    if (this.value.length > 6) {
-        this.value = this.value.slice(0,6)
-    }
-}
+let input = document.querySelector(".input");
+
+input.addEventListener('input', convert)
+input.addEventListener('input', limitCharacters)
+
+
 
 function convert () {
-    let conversionNumber = document.getElementById('conversion-input').value;
-    let metricToImperialLength = conversionNumber * 3.281
-    let metricToImperialMass = conversionNumber * 2.205
-    let metricToImperialVolume = conversionNumber / 3.78541
-    let ImperialToMetricLength = conversionNumber / 3.281
-    let ImperialToMetricVolume = conversionNumber * 3.78541
-    let ImperialToMetricMass = conversionNumber / 2.205
-
+    let conversionValue = input.value;
+    let metricToImperialLength = conversionValue * 3.281
+    let metricToImperialMass = conversionValue * 2.205
+    let metricToImperialVolume = conversionValue / 3.78541
+    let ImperialToMetricLength = conversionValue / 3.281
+    let ImperialToMetricVolume = conversionValue * 3.78541
+    let ImperialToMetricMass = conversionValue / 2.205
     
-    document.getElementById("length-unit").textContent = conversionNumber + " meters = " + metricToImperialLength.toFixed(3) + " feet | " + conversionNumber + " feet = " + ImperialToMetricLength.toFixed(3) + " meters";
     
-    document.getElementById("volume-unit").textContent = conversionNumber + " liters = " + metricToImperialVolume.toFixed(3) + " gallons | " + conversionNumber + " gallons = " + ImperialToMetricVolume.toFixed(3) + " liters";
+    document.getElementById("length-unit").textContent = conversionValue + " meters = " + metricToImperialLength.toFixed(3) + " feet | " + conversionValue + " feet = " + ImperialToMetricLength.toFixed(3) + " meters";
+    
+    document.getElementById("volume-unit").textContent = conversionValue + " liters = " + metricToImperialVolume.toFixed(3) + " gallons | " + conversionValue + " gallons = " + ImperialToMetricVolume.toFixed(3) + " liters";
+    
+    document.getElementById("mass-unit").textContent = conversionValue + " kilos = " + metricToImperialMass.toFixed(3) + " pounds | " + conversionValue + " pounds = " + ImperialToMetricMass.toFixed(3) + " kilos";
+    
+}
 
-    document.getElementById("mass-unit").textContent = conversionNumber + " kilos = " + metricToImperialMass.toFixed(3) + " pounds | " + conversionNumber + " pounds = " + ImperialToMetricMass.toFixed(3) + " kilos";
+function limitCharacters() {
+    if (input.value.length > 6) {
+        input.value = input.value.slice(0,6);
+    }
 
 }
+
